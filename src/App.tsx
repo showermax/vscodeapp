cfda68bfb2627d2c56aca61c78fee295c2d02e2d
showermax <sevenhauz@gmail.com>
2023-02-28 22:13:23 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Header } from './site/header';
+import { Body } from './site/body';
+import { Footer } from './site/footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title={'The title of my header'}/>
+      <Body />
+      <Footer />
+    </>
   );
 }
 
