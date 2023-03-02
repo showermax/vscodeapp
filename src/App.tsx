bc6e3eeb8a0665280a00b4d87ca87eed39248451
showermax <sevenhauz@gmail.com>
2023-03-02 11:55:41 +0300

    usestate

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Header } from './site/header';
@@ -6,24 +6,25 @@ import { Button } from './components/Button';
 
 
 function App() {
-  const Foo1 = (subscriber: string, age: number) => {
-    console.log(subscriber, age);
-  }
-  const Foo2 = (subscriber: string) => {
-    console.log(subscriber);
-  }
-  const Foo3 = () => {
-    console.log("I'm stupid button");
-  }
+let [a, setA]=useState(1)
+const onClickHandler = ()=>{
+  setA(++a)
+  console.log(a);
+  
+}
+const onClickHandler0 = ()=>{
+  setA(a=0)
+  console.log(a);
+  
+}
   return (
     <div className="App">
-      <button> Youtube Channel 1</button>
-      <button> Youtube Channel 2</button>
-      <Button name={"Youtube Channel 1"} callback={() => Foo1("I'm Vasya", 21)} />
-      <Button name={"Youtube Channel 2"} callback={() => Foo2("I'm Ivan")} />
-      <Button name={"Youtube Channel 1"} callback={Foo3} />
+      <h1>{a}</h1>
+      <button onClick={onClickHandler}>number</button>
+      <button onClick={onClickHandler0}>0</button>
     </div>
   )
+  
 }
 
 export default App;
@@ -38,3 +39,22 @@ export default App;
 
   // <button onClick={subscriber("Ivan")}>button text</button>
   // <button onClick={subscriber("Vasya")}>button text</button>
+
+  // const Foo1 = (subscriber: string, age: number) => {
+  //   console.log(subscriber, age);
+  // }
+  // const Foo2 = (subscriber: string) => {
+  //   console.log(subscriber);
+  // }
+  // const Foo3 = () => {
+  //   console.log("I'm stupid button");
+  // }
+  // return (
+  //   <div className="App">
+  //     <button> Youtube Channel 1</button>
+  //     <button> Youtube Channel 2</button>
+  //     <Button name={"Youtube Channel 1"} callback={() => Foo1("I'm Vasya", 21)} />
+  //     <Button name={"Youtube Channel 2"} callback={() => Foo2("I'm Ivan")} />
+  //     <Button name={"Youtube Channel 1"} callback={Foo3} />
+  //   </div>
+  // )
